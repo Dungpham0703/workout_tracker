21c97d48f93e12c4d04aa9ad8d5b1244bdb7a531
Pham Thanh Dung <phamthanhdung1112@gmail.com>
2025-09-16 14:26:38 +0700

    update API

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const API_BASE = import.meta.env.PROD
-  ? "renhttps://workout-tracker-37va.onrender.com"
+  ? "https://workout-tracker-37va.onrender.com/"
   : "";
 
 export default function WorkoutForm() {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import WorkoutForm from "../components/WorkoutForm";
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const API_BASE = import.meta.env.PROD
-  ? "renhttps://workout-tracker-37va.onrender.com"
+  ? "https://workout-tracker-37va.onrender.com/"
   : "";
 
 export default function Home() {
